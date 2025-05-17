ee7c188564cdb06cc321dc373ed122e8605536c5
valenzy-wkkr <jvalenciamunoz808@gmail.com>
2025-05-17 11:23:04 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+function obtenerFrase(){
+    document.getElementById("quote").innerHTML = "Cargando frase...";
+    document.getElementById("author").innerHTML = "";
+
+    fetch('https://api.kanye.rest/')
+    .then((response) => {
+        if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        return response.json();
+    })
+    .then((data) =>{
+        document.getElementById("quote").textContent = `"${data.quote}"`
+        document.getElementById("author").textContent = "- Kanye West"
+    })
+    .catch((error) =>{
+        console.error("Error al obtener la frase:", error)
+
+        document.getElementById("quote").textContent = "Error al obtener la frase"
+        document.getElementById("author").textContent = ""
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("button").addEventListener("click", obtenerFrase)
+})
+
+console.log("Script cargado correctamente");
